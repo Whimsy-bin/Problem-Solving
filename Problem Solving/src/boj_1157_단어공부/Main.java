0d94474f8f9804468c0071a00124058e8023848e
SSAFY <SSAFY@DESKTOP>
2022-09-08 12:09:31 +0900

    test

--- Problem Solving/src/boj_1157_단어공부/Main.java
+++ Problem Solving/src/boj_1157_단어공부/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
 //	알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
-
+//tet
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
